64b67bcca301208924618a55d2ace16738ee8a2c
unknown <madsbjergdk@gmail.com>
2023-02-01 10:05:36 +0100

    lmao

--- Lektion02/Lektion02/Fibonacci.cs
+++ Lektion02/Lektion02/Fibonacci.cs
@@ -8,30 +8,31 @@ namespace Exercise1
 {
     internal class Fibonacci
     {
+        public int X { get; private set; }
+        public int Y { private get; set; }
+
+
         public int[] FibLoop(int n)
         {
             int[] seq = new int[n];
             seq[0] = 0;
-
             if(n != 1) seq[1] = 1;
 
             for (int i = 2; i < n; i++)
-            {
                 seq[i] = seq[i - 1] + seq[i - 2];
-            }
 
             return seq;
         }
 
-        //public int[] FibRecursive(int n)
-        //{
-        //    return new int[];
-        //}
+        public int[] FibRecursive(int n)
+        {
+            int[] res = new int[n];
+            return res;
+        }
 
-        //public int Fib()
-        //{
+        public int Fib(int last, int current, int n)
+        {
 
-        //}
-
+        }
     }
 }
