bce6995747725f53d1c936084702cf9cf566378d
unknown <madsbjergdk@gmail.com>
2023-02-07 11:44:03 +0100

    Opgave1 lektion 4 done.

---
+++ Lektion4/Opgave1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Opgave1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Kort";
+            string s2 = "Lang string";
+            Console.WriteLine($"Er string 1 for lang? {s1.Lang()}");
+            Console.WriteLine($"Er string 2 for lang? {s2.Lang()}");
+            Console.WriteLine($"Er string 1 længere end 2? {s1.LangSpecifik(2)}");
+        }
+    }
+}
---
+++ Lektion4/Opgave1/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave1
+{
+    public static class StringExtensions
+    {
+        public static bool Lang(this string str)
+        {
+            return str.Length > 5;
+        }
+
+        public static bool LangSpecifik(this string str, int max)
+        {
+            return str.Length > max;
+        }
+    }
+}
