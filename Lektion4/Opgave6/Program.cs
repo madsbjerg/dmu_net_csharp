c49fe8b3178261c6b35d9c6825e257f11a9e0fdb
unknown <madsbjergdk@gmail.com>
2023-02-08 11:36:11 +0100

    opgave 6 as delegates

--- Lektion4/Opgave6/Program.cs
+++ Lektion4/Opgave6/Program.cs
@@ -12,21 +12,33 @@
             personer.Add(new Person(42, "Alin", 50));
 
             Console.WriteLine("\nSorteret efter alder: ");
-            personer.Sort(new Person.ByAgeSorter());
-            foreach(Person p in personer)
+            //personer.Sort(new Person.ByAgeSorter());
+            personer.Sort(delegate (Person x, Person y)
+            {
+                return x.Age.CompareTo(y.Age);
+            });
+            foreach (Person p in personer)
             {
                 Console.WriteLine(p.ToString());
             }
 
             Console.WriteLine("\nSorteret efter navn: ");
-            personer.Sort(new Person.ByNameSorter());
+            //personer.Sort(new Person.ByNameSorter());
+            personer.Sort(delegate (Person x, Person y)
+            {
+                return x.Name.CompareTo(y.Name);
+            });
             foreach (Person p in personer)
             {
                 Console.WriteLine(p.ToString());
             }
 
             Console.WriteLine("\nSorteret efter weight: ");
-            personer.Sort(new Person.ByWeightSorter());
+            //personer.Sort(new Person.ByWeightSorter());
+            personer.Sort(delegate (Person x, Person y)
+            {
+                return x.Weight.CompareTo(y.Weight);
+            });
             foreach (Person p in personer)
             {
                 Console.WriteLine(p.ToString());
