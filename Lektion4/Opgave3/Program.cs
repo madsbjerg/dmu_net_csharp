5ff01fb1ac9de895369867725604f9e2cf741df5
unknown <madsbjergdk@gmail.com>
2023-02-08 09:02:17 +0100

    lmao f u alin

--- Lektion4/Opgave3/Program.cs
+++ Lektion4/Opgave3/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Factorial fac = new Factorial()
+            Factorial fac = new Factorial();
             Console.WriteLine(fac.FacRecurse(10));
             Console.WriteLine(fac.FacRecurse(0));
 
---
+++ Lektion4/Opgave4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Opgave4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
