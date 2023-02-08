0aa1fe1ab67dbbe70f75a37f3ff8efca42bbcafe
unknown <madsbjergdk@gmail.com>
2023-02-08 11:27:59 +0100

    cardgame cleanup

--- Lektion4/Opgave4/CardGame.cs
+++ Lektion4/Opgave4/CardGame.cs
@@ -13,21 +13,14 @@ namespace Opgave5
         private List<Card> _cards = new List<Card>();
 
         public delegate bool FilterCardGame(Card card);
-        private FilterCardGame _filter;
 
         public void AddCard(Suit suit, Number number)
         {
             _cards.Add(new Card(suit, number));
         }
 
-        public void SetFilter(FilterCardGame filter)
-        {
-            _filter = filter;
-        }
-
         public List<Card> FilterGame(FilterCardGame filter)
         {
-            _filter = filter;
             List<Card> filtered = new List<Card>();
 
             foreach(Card card in _cards)
@@ -37,8 +30,5 @@ namespace Opgave5
 
             return filtered;
         }
-
-
-
     }
 }
